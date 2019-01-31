13fc8bc9b9b9d1e45ca66bad9d61bc94be8f19bd
crespabs <45447792+crespabs@users.noreply.github.com>
2019-01-31 16:12:09 +0800

    Basic Calculator

---
+++ Calculator/calculate.js
@@ -0,0 +1,33 @@
+function add(char) {
+	var screen = document.getElementById('screen');
+	screen.value = screen.value+char;	
+}
+function subtract(char) {
+	var screen = document.getElementById('screen');
+	screen.value = screen.value-char;
+}
+function multiply(char) {
+	var screen = document.getElementById('screen');
+	screen.value = screen.value*char;
+}
+function divide(char) {
+	var screen = document.getElementById('screen');
+	screen.value = screen.value/char;
+}
+function modulo(char) {
+	var screen = document.getElementById('screen');
+	screen.value = screen.value%char;
+}
+function backspace() {
+	var screen = document.getElementById('screen');
+	screen.value = screen.value+char;
+}
+function reset() {
+	document.getElementById('screen').value="";
+	document.getElementById('result').value="";
+}
+function equal() {
+	var screen = document.getElementById('screen');
+	var result = eval(screen.value);
+	document.getElementById('result').value=result;
+}
