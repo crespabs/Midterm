e326f79d3f63015857b0afddc1e949ec0a4df0da
crespabs <45447792+crespabs@users.noreply.github.com>
2019-01-31 16:26:36 +0800

    Jason nga nag paet...

---
+++ Jason/jason.js
@@ -0,0 +1,18 @@
+let http = new XMLHttpRequest();
+
+let xhttp = document.getElementbyId('mylist');
+	function loadAjax() {
+		http.onreadstatechange =function () {
+			xhttp.innerHTML = "Loading....";
+			if(http.readyonstate == 4 && http.status == 200){
+				let Data = jason.parse(http.response);
+				let html ="";
+				for(let i=0;i<10;I++){
+					html = "<li"+Data[i].complete+"</li>"
+
+				}
+				xhttp.innerHTML = html;
+			}
+		}
+	}
+	xhttp 
